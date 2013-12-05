46f30d8dea077c6e0f8f45b48da9464e529b67db
halvallaawaits <joereim@gmail.com>
2013-12-05 10:40:32 -0800

    Signed-off-by: halvallaawaits <joereim@gmail.com>

--- Projectv2/main.cpp
+++ Projectv2/main.cpp
@@ -6,14 +6,15 @@
 
 //Libraries
 #include <iostream>
+#include <string>
 using namespace std;
 
 //No Globals
 
 //Function Prototypes
-void gameBoard(char []);//Draws the game board
+void gameBoard(char [],string,string);//Draws the game board
 char plyrTrn(int &);
-char gtSpc(int);
+char gtSpc(int, string, string);
 void mrkSpc(char [],char,char,int &);
 int gmOvr(char [],int &);
 //int kpScr();
@@ -29,24 +30,49 @@ int main(int argc, char *argv[])
     char choice='y'; //Play again?
     int state=0;
     int player=1; //Determines which player's turn is happening
+    int numPlyr; //holds number of players
+    int gmNum=1; //holds number of games played
     char plyrMrk='X'; //Bases the X or O off player #
+    string p1Name; //holds player 1 name
+    string p2Name="Computer"; //holds player 2 name
     
     //Introduce game
-    cout<<"Play Tic Tac Toe!"<<endl;
-    cout<<"This game is meant for two players."<<endl;
+    cout<<"Welcome to Tic Tac Toe!"<<endl;
+    cout<<"Now with AI!"<<endl<<endl;
+    
+    //prompt for number of players or AI opponent selection
+    cout<<"Enter 1 for Easy AI opponent"<<endl;
+    cout<<"Enter 2 for Hard AI opponent"<<endl;
+    cout<<"Enter 3 for 2 player mode"<<endl;
+    
+    do{
+       cin>>numPlyr;
+    }while(numPlyr<1||numPlyr>3);
+    if(numPlyr==1||numPlyr==2){
+        cout<<"Enter Player 1's name: ";
+        cin>>p1Name;
+    }
+    else{
+        cout<<"Enter Player 1's name: ";
+        cin>>p1Name;
+        cout<<"Enter Player 2's name: ";
+        cin>>p2Name;
+    }
     
     //Outer loop to determine if you want to play again
     do{
         //Loop until victory or draw
         do{
+            system("CLS");
+            cout<<"Game "<<gmNum<<endl;
             //Draw Board
-            gameBoard(board);
+            gameBoard(board,p1Name,p2Name);
        
             //Determine which player's turn it is
             plyrMrk=plyrTrn(player);
        
             //Get Board Space selection from player
-            space=gtSpc(player);
+            space=gtSpc(player, p1Name, p2Name);
        
             //Mark Game Board with selection
             mrkSpc(board,space,plyrMrk,player);
@@ -55,8 +81,9 @@ int main(int argc, char *argv[])
             state=gmOvr(board,player);
        
         }while(state==0);
+        
         //Show Board one last time for end status
-        gameBoard(board);
+        gameBoard(board,p1Name,p2Name);
     
         //Show end results of game, decrement to show last player was winner
         if (state==1)
@@ -79,12 +106,17 @@ int main(int argc, char *argv[])
         board[8]='8';
         board[9]='9';
         
-    }while (choice=='y'||choice=='Y');//runs again if yes
+        //keeps track of how many subsequent games have been played
+        gmNum++;
     
+    //runs again if yes    
+    }while (choice=='y'||choice=='Y');
+        
     system("PAUSE");
     return EXIT_SUCCESS;
-}
+}//End of Main
 
+//determines if the  game is over
 int gmOvr(char a[],int &p){
     int s=0;
     
@@ -118,6 +150,7 @@ int gmOvr(char a[],int &p){
     return s;
 }
 
+//marks the space the player selects
 void mrkSpc(char a[],char sp,char mrk,int &p){
      if (sp=='1'&&a[1]=='1')
           a[1]=mrk;
@@ -141,15 +174,31 @@ void mrkSpc(char a[],char sp,char mrk,int &p){
           cout<<"Space already taken! Select a space without an X or O!"<<endl;
           p--;//decrement player so that it runs again for the same player
      }
+     system("CLS");
 }
 
-char gtSpc(int p){
-     char sp;
-     cout<<"Player "<<p<<" make your selection by typing the space number: ";
-     cin>>sp;
+//get player's selection for space
+char gtSpc(int p, string p1, string p2){
+     string space;//holds space typed by player
+     char sp;//holds actual char value of first digit in string
+     do{
+        if(p==1)
+            cout<<p1<<", make your selection by typing the space number: ";
+        else
+            cout<<p2<<", make your selection by typing the space number: ";
+        //reads in a string
+        cin>>space;
+        //truncates the string and takes only the first character in the string
+        sp=space[0];
+        if(sp!='1'&&sp!='2'&&sp!='3'&&sp!='4'&&
+           sp!='5'&&sp!='6'&&sp!='7'&&sp!='8'&&sp!='9')
+            cout<<"Invalid selection!"<<endl;
+     }while(sp!='1'&&sp!='2'&&sp!='3'&&sp!='4'&&
+           sp!='5'&&sp!='6'&&sp!='7'&&sp!='8'&&sp!='9');
      return sp;
 }
 
+//determine which player's turn it is
 char plyrTrn(int &p){
      if (p%2==1){
          p=1;
@@ -161,9 +210,9 @@ char plyrTrn(int &p){
      }
 }
 
-//This function draws the game board
-void gameBoard(char a[]){
-    cout<<"\nPlayer 1 is X's and Player 2 is O's."<<endl<<endl;
+//draws the game board
+void gameBoard(char a[],string p1, string p2){
+    cout<<"\n"<<p1<<" is X's and "<<p2<<" is O's."<<endl<<endl;
     cout<<"     ___ ___ ___ "<<endl;
     cout<<"    |   |   |   |"<<endl;
     cout<<"    | "<<a[7]<<" | "<<a[8]<<" | "<<a[9]<<" |"<<endl;
